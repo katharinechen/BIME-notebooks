b5a4f84a3c060b83757e0daafaf7c9d14d7a6f11
alangrafu <alvaro@graves.cl>
2016-12-01 09:23:24 -0800

    first import

---
+++ utils.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+# In[ ]:
+
+from random import random
+
+import pymongo
+import numpy as np
+import pandas as pd
+
+_client = pymongo.MongoClient('localhost', 27017)
+_db = _client['ghs_thalamus']
+
+def getInterviewArchive():
+    return _db.interview_archive
+
+def setPlotly():
+    import plotly
+    plotly.offline.init_notebook_mode(connected=True)
+    from plotly.graph_objs import Box, Histogram, Layout, Figure, Bar, Scatter, Marker
+    return plotly, Box, Histogram, Layout, Figure, Bar, Scatter, Marker
+
+allowedStatus = ["abandoned", "completed", "exitPointAbandoned", "rejected", "scheduleCompleted", "scheduleRejected", "startedEpilogue"]
+allowedDomains = {'greenville': ["-greenville-smartexam-com-443", "greenville-smartexam-com-443"]}
+
