9f36dd18f3f37f2946daa43b870bc7cd87f4b7ed
alangrafu <alvaro@graves.cl>
2016-12-02 13:26:55 -0800

    Added common.py files to load domains and statuses

---
+++ dev/partner-metrics/common.py
@@ -0,0 +1,5 @@
+#custom code and variables
+
+allowedStatus = ["abandoned", "completed", "exitPointAbandoned", "rejected", "scheduleCompleted", "scheduleRejected", "startedEpilogue"]
+allowedDomains = ["-greenville-smartexam-com-443", "greenville-smartexam-com-443"]
+
---
+++ stable/palmetto/common.py
@@ -0,0 +1,5 @@
+#custom code and variables
+
+allowedStatus = ["abandoned", "completed", "exitPointAbandoned", "rejected", "scheduleCompleted", "scheduleRejected", "startedEpilogue"]
+allowedDomains = ["-palmetto-smartexam-com-443", "palmetto-smartexam-com-443"]
+
